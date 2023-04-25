38b85eea9335ad99670468abf13a5dd38523d468
Julian Lescano <lescano.julian22@gmail.com>
2023-04-25 10:05:34 -0300

    fixed routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,9 +4,6 @@ import Layout from '../components/Layout';
 import Home from '../pages/Home';
 import Products from '../pages/Products';
 import PrivateRoute from './privateRoute';
-import AdminRoutes from './admin';
-import AuthRoutes from './auth';
-import Categories from '../pages/Categories';
 import Login from '../components/login';
 
 function App() {
@@ -19,7 +16,6 @@ function App() {
               <Route path="/" element={<Home />}></Route>
               <Route path="/productos" element={<Products />}></Route>
               <Route path="/login" element={<Login />}></Route>
-              {/* <Route path='/administracion/*' element={<AdminRoutes />}></Route> */}
               <Route path='/administracion/*' element={<PrivateRoute />}></Route>
           </Routes>
         </Layout>
